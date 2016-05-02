48707a610ecfb61b7077c7fa13e288bd513a4e4f
NeisesMike <neisesmichael@gmail.com>
2016-05-02 17:15:31 -0500

    Add comments, perform housekeeping

--- lotusSolver.hs
+++ lotusSolver.hs
@@ -4,6 +4,10 @@ import Data.List
 import Data.Char
 import Data.String
 
+-----------------------------------------------------------------------------------------------------------
+--Begin static declarations
+-----------------------------------------------------------------------------------------------------------
+
 --Rings of like shape, outside to inside
 ring0 = [0,1,2,3,4,5,6]
 ring1 = [7,8,9,10,11,12,13]
@@ -31,22 +35,32 @@ cw_spiral4 = [46,39,33,26,20,13,0]
 cw_spiral5 = [47,40,34,27,14,7,1]
 cw_spiral6 = [48,41,28,21,15,8,2]
 
-myList = ring0++ring1++ring2++ring3++ring4++ring5++ring6
-
+--goodList is a sample given by Dain Vermaak
 goodList::[Int]
 goodList = [5,4,7,2,1,6,3,6,5,4,3,7,2,1,7,3,6,2,1,5,4,2,1,7,5,4,6,3,1,5,4,3,6,7,2,7,6,2,1,3,5,4,3,5,4,7,2,1,6]
 
+--badList is the same as goodList, but the first index is 1 instead of 5, rendering the entire lotus invalid
+badList::[Int]
+badList = [1,4,7,2,1,6,3,6,5,4,3,7,2,1,7,3,6,2,1,5,4,2,1,7,5,4,6,3,1,5,4,3,6,7,2,7,6,2,1,3,5,4,3,5,4,7,2,1,6] 
 
-main = putStrLn $  show  myList
+-----------------------------------------------------------------------------------------------------------
+--End static declarations
+-----------------------------------------------------------------------------------------------------------
+
+main = putStrLn $ show goodList
 
 lotusSolver::[Int] -> [Int]
 lotusSolver bigList = bigList
 
+--addToList places a new value "value" at the index "index" of the list "bigList", and returns the new list
 addToList::[Int] -> Int -> Int -> [Int]
 addToList bigList index value
  |(bigList!!index)/=0 = bigList
  |otherwise = buildList bigList (-1) index value
  
+--buildList is used by addToList to construct the new list.
+--It copies all values of the original list until it reaches the desired index, at which it adds the new value.
+--It then copies all remaining values of the original list
 buildList::[Int] -> Int -> Int -> Int -> [Int]
 buildList bigList loopIndex index value
  | loopIndex==49 = []
@@ -68,20 +82,28 @@ checkAndRecurse bigList valid index
  | otherwise = [0]
  where listToCheck = (addToList bigList index)
 
+--check is used to verify the ENTIRE lotus
 check::[Int] -> Bool
 check list = (checkRing list) && (checkCCW list) && (checkCW list)
 
+--checkRing verifies that all rings are legal
 checkRing::[Int] -> Bool
 checkRing list = (compareValues list ring0) && (compareValues list ring1) && (compareValues list ring2) && (compareValues list ring3) && (compareValues list ring4) && (compareValues list ring5) && (compareValues list ring6)
 
+--checkCCW verifies that all CCW spirals are legal
 checkCCW::[Int] -> Bool
 checkCCW list = (compareValues list ccw_spiral0) && (compareValues list ccw_spiral1) && (compareValues list ccw_spiral2) && (compareValues list ccw_spiral3) && (compareValues list ccw_spiral4) && (compareValues list ccw_spiral5) && (compareValues list ccw_spiral6)
 
+--checkCW verifies that all CW sprials are legal
 checkCW::[Int] -> Bool
 checkCW list = (compareValues list cw_spiral0) && (compareValues list cw_spiral1) && (compareValues list cw_spiral2) && (compareValues list cw_spiral3) && (compareValues list cw_spiral4) && (compareValues list cw_spiral5) && (compareValues list cw_spiral6)
 
+--compareValues checks if the values of "list" at the indices contained in "indices" span 1 through 7.
+--compareValues does NOT check if two values are equal.
+--Hence, a lotus with multiple zeros will return as false.
 compareValues::[Int] -> [Int] -> Bool
-compareValues list indices = True
+compareValues list indices = sort partialList == [1,2,3,4,5,6,7]
+ where partialList = (list!!(indices!!0)):(list!!(indices!!1)):(list!!(indices!!2)):(list!!(indices!!3)):(list!!(indices!!4)):(list!!(indices!!5)):(list!!(indices!!6)):[]
 
 
 
